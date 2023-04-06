88b18da0447f612b1ed49ce669a82f843a6758e3
jaemin lee <jemin0994@gmail.com>
2023-04-06 23:13:41 +0900

    April_6th

--- OLC/OLC/ShadowCasting2D.cpp
+++ OLC/OLC/ShadowCasting2D.cpp
@@ -45,6 +45,8 @@ private :
 
 	std::vector<sEdge> vecEdges;
 
+	std::vector<std::tuple<float, float, float>> vecVisibilityPolygonPoints;
+
 	void ConvertTileMapToPolyMap(int sx, int sy, int w, int h, float fBlockWidth, int pitch) {
 		//Clear "PloyMap"
 		vecEdges.clear();
@@ -209,9 +211,82 @@ private :
 	}
 
 	void CalculateVisibilityPolygon(float ox, float oy, float radius) {
-	
-		
+		//	Get rid of existing polygon
+		vecVisibilityPolygonPoints.clear();
 
+		// For each edge in Polygon
+		for (auto& e1 : vecEdges) {
+			// Take the start point, then the end point (we could use a
+			// non-duplicated points here, it would be more optiaml)
+			for (int i = 0; i < 2; i++) {
+				
+				float rdx, rdy;
+				rdx = (i == 0 ? e1.sx : e1.ex) - ox;
+				rdy = (i == 0 ? e1.sy : e1.ey) - oy;
+			
+				float base_ang = atan2f(rdy, rdx);
+
+				float ang = 0;
+				// For each point, cast 3 rays, 1 directly at point
+				// and 1 a little bit either side
+				for (int j = 0; j < 3; j++) {
+				
+					if (j == 0) ang = base_ang - 0.0001f;
+					if (j == 1) ang = base_ang;
+					if (j == 2) ang = base_ang + 0.0001f;
+
+					//Create ray along angle for required distance
+					rdx = radius * cosf(ang);
+					rdy = radius * sinf(ang);
+
+					float min_t1 = INFINITY;
+					float min_px = 0, min_py = 0, min_ang = 0;
+
+					// Check for ray intersection with all edges
+					for (auto& e2 : vecEdges) {
+						//Create line segment vector
+						float sdx = e2.ex - e2.sx;
+						float sdy = e2.ey - e2.sy;
+
+						if (fabs(sdx - rdx) > 0.0f && fabs(sdy - rdy) > 0.0f) {
+							// t2 is normalize distance from line segment start to line segment end of intersect point
+							float t2 = (rdx * (e2.sy - oy) + (rdy * (ox - e2.sx)) / (sdx * rdy - sdy * rdx));
+							// t1 is normalize distance from source along ray to ray length of intersect point
+							float t1 = (e2.sx + sdx * t2 - ox) / rdx;
+
+							// If intersect point exist along ray, and along line
+							// segment then interscet point is valid
+							if (t1 > 0 && t2 >= 0 && t2 <= 1.0f) {
+								
+								// Check if this intersect point is closset to source.
+								// If it is, then store this point and reject others
+								if (t1 < min_t1) {
+									
+									min_t1 = t1;
+									min_px = ox + rdx * t1;
+									min_py = oy + rdy * t1;
+									min_ang = atan2f(min_py - oy, min_px - ox);
+									
+								}
+							}
+						}
+					}
+
+					// Add intersection point to visibility polygon perimeter
+					vecVisibilityPolygonPoints.push_back({ min_ang, min_px, min_py});
+				}
+			}
+		}
+
+		// Sort perimeter points by angle from source. This will allow
+		// us to draw a triangle fan
+		std::sort(
+			vecVisibilityPolygonPoints.begin(),
+			vecVisibilityPolygonPoints.end(),
+			[&](const std::tuple<float, float, float>& t1, const std::tuple<float, float, float>& t2) {
+
+				return std::get<0>(t1) < std::get<0>(t2);
+			});
 	}
 public :
 
