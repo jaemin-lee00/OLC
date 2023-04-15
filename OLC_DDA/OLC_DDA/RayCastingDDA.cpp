913c7b45ef0e945e42b2e1161bb3cb8f329d2560
jaemin lee <jemin0994@gmail.com>
2023-04-15 23:34:59 +0900

    April_15th

--- OLC_DDA/OLC_DDA/RayCastingDDA.cpp
+++ OLC_DDA/OLC_DDA/RayCastingDDA.cpp
@@ -1,8 +1,16 @@
 #define OLC_PGE_APPLICATION
-#include "olcPixelGameEngine.h"
+#include "C:\olcPixelGameEngine.h"
+
+class Example : public olc::PixelGameEngine{
+public:
+	Example() {
+	
+	}
+
+
+};
 
 int main() {
-
 
 	return 0;
 }
