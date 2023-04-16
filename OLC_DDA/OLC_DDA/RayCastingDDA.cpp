b91ed24b3a4e126acb6b39cbdc510c16755018c6
jaemin lee <jemin0994@gmail.com>
2023-04-16 22:31:54 +0900

    April_16th

--- OLC_DDA/OLC_DDA/RayCastingDDA.cpp
+++ OLC_DDA/OLC_DDA/RayCastingDDA.cpp
@@ -4,10 +4,18 @@
 class Example : public olc::PixelGameEngine{
 public:
 	Example() {
-	
+		
 	}
 
+	olc::vf2d vPlayer = { 0,0 };
+	olc::vi2d vMapSize = { 32,30 };
+	olc::vi2d VCellSize  = { 16,16 };
+	olc::vf2d vPlayer = { 0,0 };
 
+public:
+	bool OnUserCreate() override {
+		// Construct Map
+	}
 };
 
 int main() {
