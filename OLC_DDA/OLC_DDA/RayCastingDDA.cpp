972cdf9ce265909ef53ae7b19b570bd3b87dbe8b
jaemin lee <jemin0994@gmail.com>
2023-04-17 18:15:49 +0900

    April_17th

--- OLC_DDA/OLC_DDA/RayCastingDDA.cpp
+++ OLC_DDA/OLC_DDA/RayCastingDDA.cpp
@@ -4,18 +4,25 @@
 class Example : public olc::PixelGameEngine{
 public:
 	Example() {
-		
+		sAppName = "RayCast With DDA Algorithm";
 	}
 
 	olc::vf2d vPlayer = { 0,0 };
 	olc::vi2d vMapSize = { 32,30 };
 	olc::vi2d VCellSize  = { 16,16 };
-	olc::vf2d vPlayer = { 0,0 };
+	std::vector<int> vecMap;
 
 public:
 	bool OnUserCreate() override {
 		// Construct Map
+		vecMap.resize(vMapSize.x * vMapSize.y);
+		return true;
 	}
+
+	bool OnUserUpdate(float fElapsedTime) override {
+	
+	}
+
 };
 
 int main() {
