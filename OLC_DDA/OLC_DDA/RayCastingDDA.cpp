7da2fe0ae7675f8ac26ae3e04ed4f11ace3970b0
jaemin lee <jemin0994@gmail.com>
2023-04-18 15:29:12 +0900

    April_18th

--- OLC_DDA/OLC_DDA/RayCastingDDA.cpp
+++ OLC_DDA/OLC_DDA/RayCastingDDA.cpp
@@ -1,31 +1,44 @@
 #define OLC_PGE_APPLICATION
 #include "C:\olcPixelGameEngine.h"
 
-class Example : public olc::PixelGameEngine{
-public:
+class Example : public olc::PixelGameEngine {
+
+public :
 	Example() {
+	
 		sAppName = "RayCast With DDA Algorithm";
 	}
-
-	olc::vf2d vPlayer = { 0,0 };
-	olc::vi2d vMapSize = { 32,30 };
-	olc::vi2d VCellSize  = { 16,16 };
-	std::vector<int> vecMap;
-
-public:
-	bool OnUserCreate() override {
-		// Construct Map
-		vecMap.resize(vMapSize.x * vMapSize.y);
-		return true;
-	}
-
-	bool OnUserUpdate(float fElapsedTime) override {
-	
-	}
-
 };
 
-int main() {
 
-	return 0;
-}
+//class Example : public olc::PixelGameEngine{
+//public:
+//	Example() {
+//		sAppName = "RayCast With DDA Algorithm";
+//	}
+//
+//	olc::vf2d vPlayer = { 0,0 };
+//	olc::vi2d vMapSize = { 32,30 };
+//	olc::vi2d vCellSize  = { 16,16 };
+//	std::vector<int> vecMap;
+//
+//public:
+//	bool OnUserCreate() override {
+//		 Construct Map
+//		vecMap.resize(vMapSize.x * vMapSize.y);
+//		return true;
+//	}
+//
+//	bool OnUserUpdate(float fElapsedTime) override {
+//		olc::vf2d vMouse = { float(GetMouseX()), float(GetMouseY()) };
+//		olc::vf2d vMouseCell = vMouse / vCellSize;
+//		olc::vi2d vCell = (olc::vi2d)vMouseCell; // implicit cast to integer, rounds down
+//
+//	}
+//
+//};
+//
+//int main() {
+//
+//	return 0;
+//}
