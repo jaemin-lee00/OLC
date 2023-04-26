67bcb7c2476cf29811c401d796ede1b98f5c44ac
jaemin lee <jemin0994@gmail.com>
2023-04-26 22:23:02 +0900

    April_26th

--- Path_Planning/Path_Planning/PathFinding.cpp
+++ Path_Planning/Path_Planning/PathFinding.cpp
@@ -1,12 +1,27 @@
 #define OLC_PGE_APPLICATION
 #include "olcConsoleGameEngine.h"
 
-class OneLoneCoder_PathFinding {
+class OneLoneCoder_PathFinding : public olcConsoleGameEngine{
+	public:
+		OneLoneCoder_PathFinding() {
+		
+		}
 
 
+	protected:
+		virtual bool OnUserCreate() {
+			return true;
+		}
+
+		virtual bool OnUserUpdate(float fElapsedTime) {
+			return true;
+		}
 };
 
 int main() {
 
+	OneLoneCoder_PathFinding game;
+	game.ConstructConsole(160, 160, 6, 6);
+	game.Start();
 	return 0;
 }
